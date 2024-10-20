aaa9c54a7b2446fbc58a5fae5c549ff5b6028b69
shahrokh0796 <shahrokh07.96@gmail.com>
2024-10-21 02:16:48 +0530

    ready to deploy 3

--- techNoteFrontend/src/app/api/apiSlice.js
+++ techNoteFrontend/src/app/api/apiSlice.js
@@ -3,7 +3,9 @@ import { setCredentials } from "../../features/auth/authSlice";
 
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://misterfasterrepairs.onrender.com',
+    baseUrl: 'https://technotebackend-api.onrender.com',
+    // baseUrl: 'https://misterfasterrepairs.onrender.com',
+    // baseUrl: "http://localhost:3500",
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token;
